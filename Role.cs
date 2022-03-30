d907befb3ec1d2bce970647e303ca4f6bbb8ed69
thanhtung0310 <thanhtung031099@gmail.com>
2022-03-30 11:18:33 +0700

    create API for User, Department, Role

---
+++ Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAPI
+{
+    public class Role
+    {
+        public int ID { get; set; }
+        public string RoleName { get; set; }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAPI
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DepartmentID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string ContactNumber { get; set; }
+        public int RoleID { get; set; }
+    }
+}
